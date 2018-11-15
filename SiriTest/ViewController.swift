69717da1819155e21106592267cb4783e026045d
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2018-11-15 12:49:49 +0100

    Initial Commit

---
+++ SiriTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriTest
+//
+//  Created by Giuseppe Travasoni on 15/11/2018.
+//  Copyright © 2018 Travasoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
